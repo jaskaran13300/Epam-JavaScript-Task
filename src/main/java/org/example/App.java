10339067691fadaa0ef6c57572ab42f20ef89b6e
Jaskaran Batra <jskrnbtr@gmail.com>
2020-07-23 22:13:24 +0530

    Add files via upload
    Epam's Javascript Task

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+// Jaskaran EPAM's TASK
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
